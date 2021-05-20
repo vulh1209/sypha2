fd708eab0364a721cddcc8aa5ca7c26ad8f0164e
RayL <lehoangvu1209@gmail.com>
2021-05-20 17:43:39 +0700

    create project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import MenuBG from './img/MenuBG.svg';
+import Banner from './img/Banner.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img id='menubg' src={MenuBG} alt='menubg'/>
+      <img id='banner' src={Banner} alt='menubg'/>
     </div>
   );
 }
