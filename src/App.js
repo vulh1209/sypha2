30cecce66bf618ba26d9c655fa8e5126769a9131
RayL <lehoangvu1209@gmail.com>
2021-05-21 12:36:59 +0700

    update main story

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 // import logo from './logo.svg';
-import MenuBG from './img/MenuBG.svg';
-import Banner from './img/Banner.svg';
-import './App.css';
+// import MenuBG from './img/MenuBG.svg';
+// import Banner from './img/Banner.svg';
+
+import Logo from "./img/Logo.svg";
+import TextClock from "./img/TextClock.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <img id='menubg' src={MenuBG} alt='menubg'/>
-      <img id='banner' src={Banner} alt='menubg'/>
+      {/* <img id='menubg' src={MenuBG} alt='menubg'/>
+      <img id='banner' src={Banner} alt='menubg'/> */}
+
+      <div id="menubg"></div>
+      <div id="wrap">
+        <div id="banner">
+          <div id="wrap-banner">
+            <img id="logo" alt="sypha" src={Logo} />
+            <div id="paragraph1">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua.
+            </div>
+          </div>
+        </div>
+        <div id="mainstory">
+          <div id="clock">
+            <img id="textclock" alt="the main story" src={TextClock} />
+          </div>
+          <div id="paragraph2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua.
+          </div>
+        </div>
+        <div id="gameplay"></div>
+        <div id="dogeagent"></div>
+      </div>
     </div>
   );
 }
